907bcd760e0968020218ba1e080526d70b49d78c
Kyle <kingkellingson@gmail.com>
2021-08-24 19:09:03 -0600

    test

---
+++ back-end/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// parse application/json
+app.use(bodyParser.json());
+
+let items = [];
+let id = 0;
+
+app.post('/api/items', (req, res) => {
+    id = id + 1;
+    let item = {
+      id: id,
+      text: req.body.text,
+      completed: req.body.completed
+    };
+    items.push(item);
+    res.send(item);
+  });
+
+app.listen(3000, () => console.log('Server listening on port 3000!'));
